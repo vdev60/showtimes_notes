f760a60e8105e1c52c5e7b2dddf5a6e6b940dada
vdev60 <dved490@gmail.com>
2024-07-17 22:53:31 +0300

    add search_by_title_or_content to note model

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,8 @@
 class Note < ApplicationRecord
   validates :title, presence: true
+
+  def self.search_by_title_or_content(query)
+    sanitized_query = ActiveRecord::Base.sanitize_sql_like(query) # to avoid SQL injection
+    where("title LIKE ? OR content LIKE ?", "%#{sanitized_query}%", "%#{sanitized_query}%")
+  end
 end
