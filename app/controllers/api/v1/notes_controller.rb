14a9017cc12d5ee7140441f0f1ec64f081251e42
vdev60 <dved490@gmail.com>
2024-07-17 21:50:31 +0300

    add /api/v1/notes/:id update endpoint

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::NotesController < ApplicationController
+  before_action :set_note, only: %i[update]
+  
   def create
     @note = Note.new(note_params)
 
@@ -9,7 +11,18 @@ class Api::V1::NotesController < ApplicationController
     end
   end
 
+  def update
+    if @note.update(note_params)
+      render json: @note
+    else
+      render json: @note.errors, status: :unprocessable_entity
+    end
+  end
+
   private 
+  def set_note
+    @note = Note.find(params[:id])
+  end
 
   def note_params
     params.require(:note).permit(:title, :content)
